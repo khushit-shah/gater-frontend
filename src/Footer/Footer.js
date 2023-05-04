676ee42f40e3bd44ab6020758fae2e30a09b7a24
Khushit Shah <khushitshah77@gmail.com>
2023-05-04 22:56:51 +0530

    gateoverflow link update

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,7 +4,7 @@ export default class Footer extends Component {
   render() {
     return (
         <footer className="h-150 bg-blue-400 text-white p-4 text-center">
-          © 2023 GateR. Special thanks to <a href="https://www.gateoverflow.com">GateOverflow.</a>
+          © 2023 GateR. Special thanks to <a href="https://www.gateoverflow.in">GateOverflow.</a>
         </footer>
     );
   }
