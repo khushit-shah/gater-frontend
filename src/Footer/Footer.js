e63788d292586561e8e7741ea8e9fbeb36d01e27
Khushit Shah <khushitshah1@gmail.com>
2025-02-14 23:13:02 +0530

    Update Footer.js

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,6 +5,8 @@ export default class Footer extends Component {
     return (
         <footer className="h-150 bg-blue-400 text-white p-4 text-center">
           © 2023 GateR. Special thanks to <a href="https://www.gateoverflow.in">GateOverflow.</a>
+
+          <a href="https://www.goclasses.in?affCode=QL2QGI"> Use the Code: <b><i>SHAH11 or QL2QGI</b></i> on the Go Classes website to get the maximum discount on their courses!</a>
         </footer>
     );
   }
